f8b2e3556aa9e65cf149a8de4a2781b434218b49
@sebastian.zapata <sebastian.zapata@geniussportsmedia.com>
2021-10-19 19:48:15 -0500

    routing example finished

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import './homepage.styles.scss';
 import Directory from "../../components/directory/directory.component";
+import { Route } from "react-router";
+
+const Hats = (props) => {
+  console.log(props);
+  return <div>
+    <h1>Hats {props.match.params.some} </h1>
+  </div>
+}
 
 const HomePage = () => (
   <div className='homepage'>
-    <Directory />
+    <Route exact path='/' component={Directory}/>
+    <Route exact path='/hats/:some' component={Hats}/>
   </div>
 );
 
