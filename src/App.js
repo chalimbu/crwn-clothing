b228455c82545897e4a359f56ef04758c0a305d3
@sebastian.zapata <sebastian.zapata@geniussportsmedia.com>
2021-10-22 21:59:42 -0500

    basic signin page and inputs layout

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
-import Header from './components/header/header.component.jsx'
+import Header from "./components/header/header.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
