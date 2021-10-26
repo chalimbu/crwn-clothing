66516493b8ede267e6c85dfe4d0dc78f02657434
@sebastian.zapata <sebastian.zapata@geniussportsmedia.com>
2021-10-25 22:26:07 -0500

    added buttons to sigin sigout and styling of siging

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -48,9 +48,10 @@ class SignIn extends React.Component {
             label="password"
             required
           />
-
+          <div className='buttons'>
           <CustomButton type="submit" >Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} >Sign in with google</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign in with google</CustomButton>
+          </div>
         </form>
       </div>
     );
