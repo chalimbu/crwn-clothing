fa8437ed9ca5062495812854622aa96f4206d16c
@sebastian.zapata <sebastian.zapata@geniussportsmedia.com>
2021-10-28 20:57:55 -0500

    saving the user in the database when log in

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component{
   constructor(){
@@ -18,10 +18,9 @@ class App extends React.Component{
   unsubscribeFromAuth=null
   
   componentDidMount(){
-    this.unsubscribeFromAuth=auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user})
-      console.log(user);
-    })
+    this.unsubscribeFromAuth=auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
+    });
   }
 
   componentWillUnmount(){
--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -4,7 +4,6 @@ import CollectionItem from "./../collection-item/collection-item.component";
 import "./preview-collection.styles.scss";
 
 const CollectionPreview = ({ title, items }) => {
-  console.log(title, items);
   return (
     <div className="collection-preview ">
       <h1 className="title">{title.toUpperCase()}</h1>
